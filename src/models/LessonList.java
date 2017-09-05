b0f888921c7430958e49b05c89ba99292690482a
Rayaan F <rfakier2002@gmail.com>
2017-09-05 14:22:19 +0200

    Added bare files for 3 intended model classes

---
+++ src/models/LessonList.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ * Class representing the LessonList object in the SignLearner tool
+ * @author  Luveshen Pillay, Christopher Mudongo, Rayaan Fakier
+ * @date 2017-09-05
+ */
+public class LessonList {
+    
+}
