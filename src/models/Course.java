7d19314368a83c2da855d43523facfe09aa599f9
Rayaan F <rfakier2002@gmail.com>
2017-09-27 12:15:05 +0200

    Added Course class for future implementation

---
+++ src/models/Course.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * Class representing the Course object in the SignLearner tool
+ * @author  Luveshen Pillay, Christopher Mudongo, Rayaan Fakier
+ * @date 2017-09-05
+ */
+public class Course {
+    
+}
